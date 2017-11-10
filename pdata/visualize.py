3e4aeb9202556ef9e8b407b340123bd70068761d
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2017-11-10 22:48:15 +0100

    Added fill_between functionality to plot_frozen_distrib

--- pdata/visualize.py
+++ pdata/visualize.py
@@ -1,5 +1,7 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from scipy.stats import multivariate_normal
+
 
 def mvnormal_distrib_map(mu, cov, observations, padding_factor=0.2, grid_prec=0.25):
     '''
@@ -64,10 +66,16 @@ def get_axis_extent(grid_x, grid_y):
 
     return left, right, bottom, top
 
-def plot_frozen_distrib(x0, x1, distrib, linspace_num=200, **plot_kvargs):
+def plot_frozen_distrib(x0, x1, distrib, linspace_num=200, fill_alpha=0.4, **plot_kvargs):
     '''
     Visualize 1D continuous distribution on the given domain range
     '''
 
     x = np.linspace(x0, x1, num=linspace_num)
-    plt.plot(x, distrib.pdf(x), **plot_kvargs)
+    y = distrib.pdf(x)
+    plt.plot(x, y, **plot_kvargs)
+
+    fb_kvargs = {'alpha': fill_alpha}
+    if 'color' in plot_kvargs:
+        fb_kvargs['color'] = plot_kvargs['color']
+    plt.fill_between(x, y, **fb_kvargs)
