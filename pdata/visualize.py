55a8ec5af356b28166006e7060d2349578214069
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2020-05-24 12:19:55 +0200

    Improve docstrings in visualize

--- pdata/visualize.py
+++ pdata/visualize.py
@@ -4,7 +4,7 @@ from scipy.stats import multivariate_normal
 
 
 def mvnormal_distrib_map(mu, cov, observations, padding_factor=0.2, grid_prec=0.25):
-    '''
+    """
     Create a discritized map of a multivariate normal distribution on domain
     defined by the observations
 
@@ -17,7 +17,7 @@ def mvnormal_distrib_map(mu, cov, observations, padding_factor=0.2, grid_prec=0.
     :param grid_prec: precision of discretization (distance between two consecutive
                       grid points)
     :return:
-    '''
+    """
 
     dim = observations.shape[1]
 
@@ -52,11 +52,11 @@ def mvnormal_distrib_map(mu, cov, observations, padding_factor=0.2, grid_prec=0.
     return grid_x, grid_y, distrib_map
 
 def get_axis_extent(grid_x, grid_y):
-    '''
+    """
     Get a tuple of (left, right, bottom, top)
     from the meshgrid output. Used for the extent
     parameter in matplotlib
-    '''
+    """
 
     left = grid_x[0, 0]
     right = grid_x[0, -1]
@@ -67,9 +67,9 @@ def get_axis_extent(grid_x, grid_y):
     return left, right, bottom, top
 
 def plot_frozen_distrib(x0, x1, distrib, linspace_num=200, fill_alpha=0.4, **plot_kvargs):
-    '''
+    """
     Visualize 1D continuous distribution on the given domain range
-    '''
+    """
 
     x = np.linspace(x0, x1, num=linspace_num)
     y = distrib.pdf(x)
