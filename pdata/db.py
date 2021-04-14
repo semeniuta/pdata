ab977e80f4181c1b8503313f4e9e85844050202c
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2021-04-14 13:54:49 +0200

    First version of SQLSelectQuery, w/ comma_separated_string

--- pdata/db.py
+++ pdata/db.py
@@ -3,6 +3,10 @@ from sqlalchemy import create_engine
 import sqlite3
 
 
+def comma_separated_string(values):
+    return ', '.join(values)
+
+
 def create_mysql_engine(host, user, password, db):
 
     url_template = 'mysql+mysqlconnector://{}:{}@{}/{}?charset=utf8'
@@ -23,3 +27,26 @@ class SQLiteDatabase:
         conn.close()
 
         return df
+
+
+class SQLSelectQuery:
+
+    def __init__(self, what, relations, conditions, name=None):
+        
+        self.what = what
+        self.relations = relations
+        self.conditions = conditions
+        self.name = name
+
+    def __str__(self):
+
+        what_s = comma_separated_string(self.what)
+        relations_s = comma_separated_string(self.relations)
+        conditions_s = comma_separated_string(self.conditions)
+
+        q = 'SELECT {} FROM {} WHERE {}'.format(what_s, relations_s, conditions_s)
+
+        if self.name is not None:
+            q = '({}) {}'.format(q, self.name)
+
+        return q
